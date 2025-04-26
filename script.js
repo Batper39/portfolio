7bae03369ff6607fe5f9c42afa44f86f91d42557
Batper39 <batelpe1@gmail.com>
2025-04-27 00:52:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove('shrink');
   }
 });
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+  hamburger.classList.toggle('open');
+});
